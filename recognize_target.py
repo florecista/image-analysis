7066191ffa38dd6cec5a78e2615a4bb7efd9d60e
Matt <attentionmatt@gmail.com>
2020-05-09 17:32:38 +1000

    Small change to filter files by extensions

--- recognize_target.py
+++ recognize_target.py
@@ -1,9 +1,9 @@
 import face_recognition
 import os
-
-source_image = face_recognition.load_image_file("images/COL-Flagg.jpg")
+import re
 
 try:
+    source_image = face_recognition.load_image_file("images/COL-Flagg.jpg")
     source_face_encoding = face_recognition.face_encodings(source_image)[0]
 
     known_faces = [
@@ -11,8 +11,9 @@ try:
     ]
 
     filelist = os.listdir('images')
-    for image_file in filelist[:]:  # filelist[:] makes a copy of filelist.
-        if (image_file.endswith(".jpg") or image_file.endswith(".JPG")):
+    pattern = re.compile(r'[.](jpg|jpeg|png)$', re.IGNORECASE)
+    for image_file in filelist[:]:
+        if re.search(pattern, image_file):
             unknown_image = face_recognition.load_image_file('images/' + image_file)
             unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
             results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
