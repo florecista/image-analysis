0bfb1d49bcf7afb8a21d9d435e9aefb0faba4cac
Matt <attentionmatt@gmail.com>
2020-05-10 16:41:29 +1000

    Adding facial recognition from video + some tidy up

--- recognise_target_image.py
+++ recognise_target_image.py
@@ -3,19 +3,19 @@ import os
 import re
 
 try:
-    source_image = face_recognition.load_image_file("images/COL-Flagg.jpg")
+    source_image = face_recognition.load_image_file("images_known/COL-Flagg.jpg")
     source_face_encoding = face_recognition.face_encodings(source_image)[0]
 
     known_faces = [
         source_face_encoding
     ]
 
-    filelist = os.listdir('images')
+    filelist = os.listdir('images_unknown')
     sorted_filelist = sorted(filelist)
     pattern = re.compile(r'[.](jpg|jpeg|png)$', re.IGNORECASE)
     for image_file in sorted_filelist[:]:
         if re.search(pattern, image_file):
-            unknown_image = face_recognition.load_image_file('images/' + image_file)
+            unknown_image = face_recognition.load_image_file('images_unknown/' + image_file)
             unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
             results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
             if results[0] == True:
@@ -24,5 +24,5 @@ try:
                 print(image_file + " does not match")
 
 except IndexError:
-    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    print("I wasn't able to locate any faces in at least one of the images_unknown. Check the image files. Aborting...")
     quit()
---
+++ recognise_target_video.py
@@ -0,0 +1,52 @@
+import face_recognition
+import cv2
+
+class recognize_target_video():
+    def findTarget(self, videofile, imagefile):
+        input_movie = cv2.VideoCapture(videofile)
+        image = face_recognition.load_image_file(imagefile)
+        face_encoding = face_recognition.face_encodings(image)[0]
+
+        known_faces = [
+            face_encoding
+        ]
+
+        frame_number = 0
+
+        result = False
+
+        while True:
+            # Grab a single frame of video
+            ret, frame = input_movie.read()
+            frame_number += 1
+            print('Processing frame number : ' + str(frame_number))
+            # Quit when the input video file ends
+            if not ret:
+                break
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_frame = frame[:, :, ::-1]
+
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_frame)
+            face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+                if match[0]:
+                    result = True
+
+        # All done!
+        input_movie.release()
+        cv2.destroyAllWindows()
+
+        return result
+
+def main():
+    app = recognize_target_video()
+    result = app.findTarget("video/obama.mp4", "images_unknown/unknown40.jpg")
+    print('Target is in video : ' + str(result))
+
+if __name__ == "__main__":
+    main()
